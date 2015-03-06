8bf8e6148b080598e736eff814b7f1b06b158a8c
dair diaz <ddiaz@himediadigital.com>
2015-03-06 11:36:56 -0500

    Plantillas de login e inicio de plantilla de dashboard con login de frontend para admin de proceso de votación.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,18 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/login", name="login")
-     */
-    public function indexAction()
-    {
-        return $this->render('default/login.html.twig');
-    }
-    
-    /**
      * @Route("/dashboard", name="dashboard")
      */
     public function dashboardAction()
     {
-        return $this->render('default/dashboard.html.twig');
+        return $this->render('AppBundle::dashboard.html.twig');
     }
 }
