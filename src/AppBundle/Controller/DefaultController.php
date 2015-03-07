f41fb74bec8d6a529533b8ab4a99571f35e2bbe0
dair diaz <ddiaz@himediadigital.com>
2015-03-07 15:10:03 -0500

    Visualización de candidatos estudiantiles y personero para un usuario de tipo estudiante. Iteracción en la seleccion del candidato para seleccionar uno de cada tipo (candidato estudiantil y personero). (Aun no se traen los datos de candidatos de la base de datos, estan quemados en HTML)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,5 +16,34 @@ class DefaultController extends Controller
     public function dashboardAction()
     {
         return $this->render('AppBundle::dashboard.html.twig');
+    }
+    
+    /**
+     * @Route("/send-form", name="send-form")
+     * @Method({"POST"})
+     */
+    public function searchFormAction(Request $request)
+    {
+        $code = $request->request->get('code', null);
+        $em = $this->getDoctrine()->getManager();
+        $object = $em->getRepository('AppBundle:Student')->findOneByCode($code);
+        $data = [
+            'state' => 'ok',
+        ];
+        if ($object) {
+            $data['template'] = $this->get('twig')->render('AppBundle::student_result.html.twig');
+            $data['target'] = 'student';
+        } else {
+            $object = $em->getRepository('AppBundle:Teacher')->findOneByCode($code);
+            if ($object) {
+                $data['template'] = $this->get('twig')->render('AppBundle::teacher_result.html.twig');
+                $data['target'] = 'teacher';
+            } else {
+                $data['state'] = 'error';
+                $data['message'] = 'El código que ha ingresado es inválido, por favor, verifique.';
+            }
+        }
+        $response = new JsonResponse($data);
+        return $response;
     }
 }
--- web/assets/js/dashboard.js
+++ web/assets/js/dashboard.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var app = {
+    owl:null,
+    preloader:null,
+    form:"#search-form",
+    searchContainer:"#search-container",
+    selected:{},
     init:function(){
         this.initCarousel();
+        this.initPreloader();
+        $(this.form).on("submit", this.searchForm);
     },
     initCarousel:function(){
         $("#owl-demo").owlCarousel({
@@ -12,6 +19,140 @@ var app = {
             autoHeight: true,
             itemsScaleUp:true
         });
+        this.owl = $(".owl-carousel").data('owlCarousel');
+    },
+    initPreloader:function(){
+        var opts = {
+            lines: 11, // The number of lines to draw
+            length: 9, // The length of each line
+            width: 2, // The line thickness
+            radius: 6, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#000', // #rgb or #rrggbb or array of colors
+            speed: 1, // Rounds per second
+            trail: 22, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner loading', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: '50%', // Top position relative to parent
+            left: '90%' // Left position relative to parent
+        };
+        this.preloader = new Spinner(opts).spin();
+    },
+    searchForm:function(event){
+        event.preventDefault? event.preventDefault(): event.returnValue = false;
+        $.ajax({
+            url:$("#send-form").val().trim(),
+            type:'post',
+            data:$(event.target).serialize(),
+            dataType:'json',
+            beforeSend:function(){
+                $(this.form).off();
+                app.showLoading();
+            },
+            success:function(response){
+                if(response.state === 'ok'){
+                    $(".content-container").fadeOut(500,function(){
+                        $(this).hide()
+                            .html(response.template)
+                            .fadeIn(500);
+                        app.owl.destroy();
+                        app.initVoting(response.target);
+                    });
+                }else {
+                    app.showModal(
+                        "Mensaje",
+                        response.message,
+                        "Aceptar"
+                    );
+                }
+            },
+            error:function(response){
+                app.showModal(
+                    "Mensaje",
+                    "Ops! parece que se ha presentado un error en el sistema, por favor intentalo más tarde :(",
+                    "Aceptar"
+                );
+            },
+            complete:function(){
+                $(this.form).on("submit", this.searchForm);
+                app.hideLoading();
+            }
+        });
+    },
+    showLoading:function(){
+        var target = $(this.searchContainer)[0];
+        target.appendChild(this.preloader.el);
+    },
+    hideLoading:function(){
+        var target = $(this.searchContainer)[0];
+        $(target).find(".spinner.loading").remove();
+    },
+    showModal:function(title, message, acceptButton){
+        var $modal = $('#modal');
+        var html = $modal.html()
+            .replace("{$title}", title)
+            .replace("{$message}", message)
+            .replace("{$acceptButton}", acceptButton);
+        $modal.html(html).modal('show');
+        $modal = html = undefined;
+    },
+    initVoting:function(target){
+        $(".owl-candidates").owlCarousel({
+            autoPlay: false,
+            items : 5,
+            autoHeight: true,
+            itemsScaleUp:true
+        });
+
+        $(".candidate.item").on("click", function(event){
+            app.selectCandidate(event, target);
+        });
+    },
+    selectCandidate:function(event, target){
+        var item = app.findObjectUp(event.target, ".item");
+        var $item = $(item);
+        var attr = $item.attr("target");
+        if(app.selected[attr]){
+            $(".candidate.item[selected][target='" + attr + "']")
+                .removeAttr("selected")
+                .find(".item-mark")
+                .removeClass("slidein")
+                .addClass("slideout");
+        }else{
+            app.selected[attr] = {
+                id:$item.attr("target-id"),
+                type:attr.split("-")[1]
+            };
+        }
+        
+        $item.find(".item-mark")
+            .removeClass("slideout")
+            .addClass("slidein");
+        $item.attr("selected", true);
+        item = undefined;
+    },
+    jQueryContains:function($jq,elem){
+        for(var i=0; i<$jq.length;i++){
+            if($jq[i]===elem)
+                return true;
+        }
+        return false;
+    },
+    findObjectUp:function(target, selector){
+        var $jq = $(selector);
+        var obj = target;
+        while(app.jQueryContains($jq,obj) === false){
+            if(obj.parentNode){
+                obj = obj.parentNode;
+            }else{
+                return null;
+            }
+        }
+        return obj;
     }
 };
 app.init();
